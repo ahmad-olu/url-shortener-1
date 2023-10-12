801a8611f32b478219118f9cd4031bef0d66c4e3
ola-lekan <78583289+ola-lekan@users.noreply.github.com>
2023-10-12 14:50:06 +0100

    added nothing

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BlogPostDetail(params) {
-  return <div>BlogPostDetail {params.params.slug}</div>;
+  return <div>BlogPostDetail now {params.params.slug}</div>;
 }
 
 export default BlogPostDetail;
